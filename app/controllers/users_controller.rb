15a9552e42342e720cfc693f1a982f153f26bd74
ro-ze1106 <roze56477@gmail.com>
2021-10-13 18:51:38 +0900

    「自動ログインにする」機能のリクエスト/システムテストを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      remember user
       flash[:success] = 'スタディログへようこそ'
       redirect_to @user
     else
