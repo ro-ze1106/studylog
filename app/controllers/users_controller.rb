9d180a32d1ac1303fecade04a0177e9b6e1ffc35
ro-ze1106 <roze56477@gmail.com>
2021-10-17 15:46:56 +0900

    フレンドリーフォワーディングの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,8 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
+        store_location
+        flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
     end
