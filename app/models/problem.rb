e494fb865f9a0ddd78ee758f69900ebfdb5d9bfb
ro-ze1106 <roze56477@gmail.com>
2021-11-17 09:59:22 +0900

    画像アップロード機能、表示部の実装

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,5 +1,6 @@
 class Problem < ApplicationRecord
   belongs_to :user
+  has_one_attached :picture
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :study_type, presence: true, length: { maximum: 30 }
