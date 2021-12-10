ba249fd10c4537c22782752005790397de126965
ro-ze1106 <roze56477@gmail.com>
2021-12-10 15:47:44 +0900

    能動的なフォロー機能とそのテストを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                    foreign_key: "follower_id",
                    dependent:  :destroy
+  has_many :following, through: :active_relationships,source: :followed
   attr_accessor :remember_token
-
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -50,5 +50,20 @@ class User < ApplicationRecord
   # フィード一覧を取得
   def feed
     Problem.where('user_id = ?', id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
