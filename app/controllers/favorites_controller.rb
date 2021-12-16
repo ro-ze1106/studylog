898a6dfe15a20b6020172c599450bb3140521f5e
ro-ze1106 <roze56477@gmail.com>
2021-12-16 16:22:33 +0900

    お気に入りのcreate/destroyアクション実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @problem = Problem.find(params[:dish_id])
+    @user = @problem.user
+    current_user.favorite(@problem)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @problem = Problem.find(params[:dish_id])
+    current_user.favorites.find_by(dish_id: @problem.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
