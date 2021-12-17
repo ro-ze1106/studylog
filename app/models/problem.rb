9648dfe64f0a8c544ccf562f42cb562b924dc05f
ro-ze1106 <roze56477@gmail.com>
2021-12-17 14:17:15 +0900

    Commentモデルの関連付け、バリデーションを追加

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,7 @@
 class Problem < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :picture
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    problem_id { 1 }
     user_id { 1 }
-    content { "MyText" }
+    content { "難しい" }
+    association :problem
   end
 end
