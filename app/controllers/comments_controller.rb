6b41807e7fd443624698535431ce17dfcd001663
ro-ze1106 <roze56477@gmail.com>
2021-12-17 15:42:32 +0900

    コメントのcreateアクションの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @problem = Problem.find(params[:problem_id])
+    @user = @problem.user
+    @comment = @problem.comments.build(user_id: current_user.id, content: params[:comment] [:content])
+    if !@problem.nil? && @comment.save
+      flash[:success] = "コメントを追加しました!"
+    else
+      flash[:danger] = "コメントを入力して下さい。"
+    end
+    redirect_to request.referrer || root_url
   end
 
   def destroy
