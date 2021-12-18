0cde00b622a3e1a913e55a9b11c49364322d54ec
ro-ze1106 <roze56477@gmail.com>
2021-12-18 22:50:40 +0900

    Notificationモデルの関連付けとバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :favorites, dependent: :destroy
+  has_many :notidications, dependent: :destroy
   attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notification) { create(:notification) }
+
+  it 'notificationsインスタンスが有効であること' do
+    expect(notification).to be_valid
+  end
+
+  it 'user_idがnilの場合、無効であること' do
+    notification.user_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it 'problem_idがnilの場合、無効であること' do
+    notification.problem_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it 'varietyがnilの場合、無効であること' do
+    notification.variety = nil
+    expect(notification).not_to be_valid
+  end
+
+  it 'from_user_idがnilの場合、無効であること' do
+    notification.from_user_id = nil
+    expect(notification).not_to be_valid
+  end
+
 end
