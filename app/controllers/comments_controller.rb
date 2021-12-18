60ca57ed2ed2f0bfa5c4b8e22164a0e5fe65291b
ro-ze1106 <roze56477@gmail.com>
2021-12-18 13:35:38 +0900

    コメントの削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,5 +14,12 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @problem = @comment.problem
+    if current_user.id == @comment.user_id
+      @comment.destroy
+      flash[:success] = "コメントを削除しました"
+    end
+    redirect_to problem_url(@problem)
   end
 end
