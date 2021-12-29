bf697bacbeccf4717c457d023e35ef3be7fc556e
ro-ze1106 <roze56477@gmail.com>
2021-12-29 10:47:20 +0900

    問題出題のテスト追加

---
+++ spec/requests/problems_question_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "問題出題", type: :request do
+  let!(:user) { create(:user) }
+  let!(:problem) { create(:problem, user: user) }
+
+  context '認可されたユーザーの場合' do
+    it 'レスポンスが正常に表示されること' do
+      login_for_request(user)
+      get question_problem_path(problem)
+      expect(response).to have_http_status "200"
+      expect(response).to render_template('problems/question')
+    end
+  end
+
+  context 'ログインされていないユーザーの場合' do
+    it 'ログイン画面にリダイレクトされること' do
+    get question_problem_path(problem)
+    expect(response).to have_http_status "302"
+    expect(response).to redirect_to login_path
+    end
+  end
+end
---
+++ spec/system/problems_question_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "ProblemsQuestion", type: :system do
+  let!(:user) { create(:user)}
+  let!(:problem) { create(:problem, user: user) }
+
+  describe '問題出題ページ' do
+    before do
+      login_for_system(user)
+      visit question_problem_path(problem)
+    end
+
+    context 'レイアウトページ' do
+      it '正しいタイトルが表示されていること' do
+        expect(page).to have_title full_title('問題')
+      end
+
+      it '必要な問題(problem)情報が表示されていること' do
+        expect(page).to have_content problem.study_type
+        expect(page).to have_content problem.explanation_text
+        expect(page).to have_content problem.problem_text
+      end
+    end
+
+    context '問題解答の処理' do
+      it '有効な情報で問題出題を行うと成功のフラッシュが表示されること' do
+        fill_in '答え', with: '94'
+        click_button '答え合わせ'
+        expect(page).to have_content '当たり'
+      end
+
+      it '無効な情報で問題出題を行うと失敗のフラッシュが表示されること' do
+        fill_in '答え', with: '0'
+        click_button '答え合わせ'
+        expect(page).to have_content 'はずれ'
+      end
+    end
+  end
+end
